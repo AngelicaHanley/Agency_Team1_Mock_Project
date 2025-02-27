65d258e7725083d58d7f9b8d113aa26704490aad
angelicahanley <hanleyangelica@ufl.edu>
2025-02-27 00:05:11 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div className="w-full h-screen p-15">
+      <img src="images/Footlong_Cookie.png" alt="Footlong Cookie"
+        className="w-full h-auto"/>
+      <h1>Subway - Agency Team 1</h1>
+
+      <div className="button">
+        <button onClick={() => setCount((count) => count + 1)} className="text-black">
+          Count: {count}
+        </button>
+  
+      </div>
+
+      <p>
+        Rest of website here!
+      </p>
+      </div>
+    </>
+  )
+}
+
+
+function Media() {
+
+  return (
+    <>
+    
+ 
+
+    </>
+  )
+}
+
+export default App
