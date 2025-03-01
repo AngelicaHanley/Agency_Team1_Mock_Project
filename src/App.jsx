c46cb382b4fd849773cce866decde8f6332afbfc
Ayah-Saleh <116742207+Ayah-Saleh@users.noreply.github.com>
2025-03-01 00:38:40 -0500

    added buttons and linked to subway page, restructured code

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,50 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-    <div className="w-full h-screen p-5">
-      <img src="images/subway_logo_white.png" alt="Subway Logo" />
-      {/*<img src="images/Footlong_Cookie.png" alt="Footlong Cookie"
-        className="w-full h-auto"/>*/}
+    <div className="w-full h-screen p-5 flex flex-col items-center justify-center text-center">
+      <img src="images/subway_logo_white.png" alt="Subway Logo" className="mb-5" />
 
-      <img src="images/SubwayCookieBittenwHint.gif" alt="Footlong Cookie"
-              className="w-full h-auto"/>
 
-      <h1>Subway - Agency Team 1</h1>
+      <h1 className="not-baked-text"><b>This site isn't baked yet...</b></h1>
 
-      <div className="button">
-        <button onClick={() => setCount((count) => count + 1)} className="text-black">
-          Count: {count}
-        </button>
-  
+      <img
+        src="images/SubwayCookieBittenwHint.gif"
+        alt="Footlong Cookie Animation"
+      />
+
+      <h1 className = "royal-text"><em>royal excellence loading...</em>.</h1>
+      <h1 className="text-lg font-semibold mb-5">Subway - Agency Team 1</h1>
+
+      <div className="flex flex-col md:flex-row gap-4">
+        <CustomButton
+          className="orderbtn"
+          text="Order Now"
+          link="https://www.subway.com/en-us"
+        />
+        <CustomButton
+          className="friendSend"
+          text="Send to a Friend"
+          link="https://www.subway.com/en-us"
+        />
       </div>
 
-      <p>
-        Rest of website here!
-      </p>
-      </div>
-    </>
-  )
+      <p>Rest of website here!</p>
+    </div>
+  );
 }
 
-
-function Media() {
-
+const CustomButton = ({ className, text, link }) => {
   return (
-    <>
-    
- 
+    <button
+      className={`${className} px-6 py-3 rounded-md font-semibold text-white`}
+      onClick={() => window.location.href = link}
+    >
+      {text}
+    </button>
+  );
+};
 
-    </>
-  )
-}
-
-export default App
+export default App;
